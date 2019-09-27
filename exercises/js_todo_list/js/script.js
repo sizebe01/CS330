4192d04d79c75aea96d526898a66a0a99d8c6bfb
sizebe01 <sizebe01@luther.edu>
2019-09-26 20:27:04 -0500

    adding exercise

--- exercises/js_todo_list/js/script.js
+++ exercises/js_todo_list/js/script.js
@@ -6,22 +6,93 @@ var team = ["Aardvark", "Beaver", "Cheetah", "Dolphin", "Elephant", "Flamingo", 
 var priority = ["Low", "Normal", "Important", "Critical"];
 
 function addTask() {
-    /* Collect valid values from the input fields and call addRow() to add a row to the table of tasks */
-}
-
-function addRow(valueList, parent) {
-    /* Create a new row element with data provided in the valueList and append this row to the table (parent) provided */
+    let rowcolids = ['taskname', 'assigned', 'priority', 'date'];
+    let vals = [];
+    for (let cid of rowcolids) {
+        vals.push(document.getElementById(cid).value);
+    }
+    makeRow(vals, document.getElementById('todolist'));
 }
 
 function removeRow() {
-    /* Remove the row of the table is a checkbox in that row is clicked (checked) */
+   
+    var checked = document.getElementsByClassName('checkbox');
+    
+    for (var i = 0; i < checked.length; i++){
+        if (checked[i].checked){
+            document.getElementById("todolist").deleteRow(i);
+            
+        }
+    }
+}
+
+function makeRow(valueList, parent) {
+    let row = document.createElement("tr");
+    row.innerHTML += " <input type = 'checkbox' onclick='removeRow()' class = 'checkbox form-control'/>";
+    
+    if(valueList.includes('')){
+        
+        window.alert('Make sure the Task and Date fields are filled out!');
+    } else{
+        
+        if(valueList.includes('Low')){
+            for (let val of valueList) {
+            
+                let td = document.createElement("td");
+                td.innerHTML = val;
+                td.style.backgroundColor = "#00A2FF";
+                row.appendChild(td);
+            }
+            parent.appendChild(row); 
+        }
+        
+        if(valueList.includes('Normal')){
+            for (let val of valueList) {
+                let td = document.createElement("td");
+                td.innerHTML = val;
+                td.style.backgroundColor = "#FFF700";
+                row.appendChild(td);
+            }
+            parent.appendChild(row); 
+        }
+        
+        if(valueList.includes('Important')){
+            for (let val of valueList) {
+                let td = document.createElement("td");
+                td.innerHTML = val;
+                td.style.backgroundColor = "#FFBC00";
+                row.appendChild(td);
+            }
+            parent.appendChild(row); 
+        }
+
+        if(valueList.includes('Critical')){
+            for (let val of valueList) {
+                let td = document.createElement("td");
+                td.innerHTML = val;
+                td.style.backgroundColor = "#FF3C00";
+                row.appendChild(td);
+            }
+            parent.appendChild(row); 
+        }
+    }
 }
 
 function populateSelect(selectId, sList) {
-    /* Populate the option (drop-down) element (selectId) with items from the sList */
+    let sel = document.getElementById(selectId);
+    for (let s of sList) {
+        let opt = document.createElement("option");
+        opt.value = s;
+        opt.innerHTML = s;
+        sel.appendChild(opt);
+    }
 }
 
-window.onload = function () {
-    populateSelect("assignedTo", team);
-    populateSelect("priority", priority);
-};
+
+$(document).ready(function () {
+    populateSelect('assigned', team);
+    populateSelect('priority', priority);
+});
+
+
+
