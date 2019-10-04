8f9ce384361f0790556a848bc2f288a1fe954f35
sizebe01 <sizebe01@luther.edu>
2019-10-03 20:53:12 -0500

    adding changes

--- projects/shopping_list/js/controller.js
+++ projects/shopping_list/js/controller.js
@@ -76,7 +76,8 @@ function populateSelect(selectId, sList) {
 }
 
 $(document).ready(function () {
-    readdrows();
+    
     populateSelect('store', stores);
     populateSelect('category', sections);
+    readdrows();
 });
