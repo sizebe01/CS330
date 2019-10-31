d34c99aaa4f651214e480827b32c96a4dcb71b17
sizebe01 <sizebe01@luther.edu>
2019-10-30 21:55:03 -0500

    saving

--- exercises/py_flask_jokes/app.py
+++ exercises/py_flask_jokes/app.py
@@ -5,10 +5,31 @@ from flask import Flask, request, render_template, url_for, make_response
 
 app = Flask(__name__)
 
+ 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
+    
+    language = ['en', 'de', 'es', 'gl' , 'eus']
+    joketype = 	['neutral', 'chuck', 'all']
+
+    return render_template('jokes.html', language = language, joketype = joketype)
     pass
 
-def send_joke(category: str, language: str, number: int):
-    pass
+@app.route("/joke", methods=["GET", "POST"])
+def joke():
+    try:
+        languagetype = str(request.form.get('language'))
+        joketype = str(request.form.get('joketype'))       
+        jokes = []
+        getjoke = pyjokes.get_joke(languagetype,joketype)
+        jokes.append(getjoke)
+        return render_template('jokes.html', jokes = jokes)
+        pass
+    except:
+        error = "This can not be translated!"
+        return render_template('jokes.html', error = error)
+
+if __name__ == "__main__":
+    app.run(debug = True)
+
