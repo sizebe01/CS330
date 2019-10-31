62fd5c3582cb498a36faeeb5d69f57b2afbd3eb3
sizebe01 <sizebe01@luther.edu>
2019-10-31 14:15:18 -0500

    changes

--- exercises/py_flask_jokes/app.py
+++ exercises/py_flask_jokes/app.py
@@ -12,22 +12,26 @@ def index():
     
     language = ['en', 'de', 'es', 'gl' , 'eus']
     joketype = 	['neutral', 'chuck', 'all']
+    number = ['1','2','3','4','5','6','7','8','9','10']
 
-    return render_template('jokes.html', language = language, joketype = joketype)
+    return render_template('jokes.html', language = language, joketype = joketype, number = number)
     pass
 
 @app.route("/joke", methods=["GET", "POST"])
 def joke():
     try:
         languagetype = str(request.form.get('language'))
-        joketype = str(request.form.get('joketype'))       
+        joketype = str(request.form.get('joketype'))  
+        number = request.form.get('number')   
         jokes = []
-        getjoke = pyjokes.get_joke(languagetype,joketype)
-        jokes.append(getjoke)
+        for i in range (int(number)):
+            getjoke = pyjokes.get_joke(languagetype,joketype)
+            jokes.append(getjoke)
+
         return render_template('jokes.html', jokes = jokes)
         pass
     except:
-        error = "This can not be translated!"
+        error = "Translation not Supported"
         return render_template('jokes.html', error = error)
 
 if __name__ == "__main__":
