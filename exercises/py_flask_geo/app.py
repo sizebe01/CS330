d126a431e46251a83ad7bbcb22eca5f83c8223e2
sizebe01 <sizebe01@luther.edu>
2019-11-14 21:37:12 -0600

    adding changes

--- exercises/py_flask_geo/app.py
+++ exercises/py_flask_geo/app.py
@@ -1,48 +1,50 @@
-from flask import Flask
+#import requests
+from flask import Flask, request, render_template, send_from_directory
 from flask import redirect, url_for
-from flask import request, make_response
-from flask import render_template
 import records
 
 app = Flask(__name__)
 
-db = records.Database("postgresql://sizebe01:@knuth.luther.edu/world")
+data = records.Database("postgresql://sizebe01:@knuth.luther.edu/world")
 
 @app.route('/')
 def index():
     
-    return redirect(url_for('country'))
+    return render_template("index.html")
+
 
 @app.route('/country', methods=['GET', 'POST'])
 def country():
-    optionsQuery = db.query("select code, name from country")
+    #Getting Country Names for dropdown menu
+    dropdown_menu = data.query("select code, name from country")
+    #Creating Query from form
     if request.method == 'POST':
         code = request.form['code']
-        #rows = db.query("select *, city.name as capitalname from country left join city on country.capital = city.id where code='" + code + "' ")
-        rows = db.query("select * from country join city on country.capital = city.id where code='" + code + "' ")
-        return render_template("results.html", code=code, options=optionsQuery, results=rows.all())
-    return render_template("results.html", options=optionsQuery.all())
+        rows = data.query("select * from country join city on country.capital = city.id where code='" + code + "' ")
+        return render_template("results.html", code=code, dropdown=dropdown_menu, results=rows.all())
+    #Populate Dropdown
+    return render_template("results.html", dropdown=dropdown_menu.all())
 
 
 @app.route('/region', methods=['GET', 'POST'])
 def region():
-    optionsQuery = db.query("select distinct region, region as dupregion from country")
+    dropdown_menu = data.query("select distinct region, region as region from country")
 
     if request.method == 'POST':
         code = request.form['code']
-        rows = db.query("select *, city.name as capitalname from country left join city on country.capital = city.id where region='" + code + "' ")
-        return render_template("results.html", code=code, options=optionsQuery.all(), results=rows.all())
-    return render_template("results.html", options=optionsQuery.all())
+        rows = data.query("select *, city.name as capitalname from country left join city on country.capital = city.id where region='" + code + "' ")
+        return render_template("results.html", code=code, dropdown=dropdown_menu.all(), results=rows.all())
+    return render_template("results.html", dropdown=dropdown_menu.all())
 
 @app.route('/continent', methods=['GET', 'POST'])
 def continent():
-    optionsQuery = db.query("select distinct continent, continent as dupcontinent from country")
+    dropdown_menu = data.query("select distinct continent, continent as continent from country")
 
     if request.method == 'POST':
         code = request.form['code']
-        rows = db.query("select *, city.name as capitalname from country left join city on country.capital = city.id where continent='" + code + "' ")
-        return render_template("results.html", code=code, options=optionsQuery.all(), results=rows.all())
-    return render_template("results.html", options=optionsQuery.all())
+        rows = data.query("select *, city.name as capitalname from country left join city on country.capital = city.id where continent='" + code + "' ")
+        return render_template("results.html", code=code, dropdown=dropdown_menu.all(), results=rows.all())
+    return render_template("results.html", dropdown=dropdown_menu.all())
 
 
 
