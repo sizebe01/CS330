024fd397fb9cac0418550c0d7a4cb2872b788ccd
sizebe01 <sizebe01@luther.edu>
2019-11-25 11:01:27 -0600

    changes

--- exercises/py_flask_security/app.py
+++ exercises/py_flask_security/app.py
@@ -1,19 +1,34 @@
-from flask import Flask, session, redirect, url_for, escape, request, render_template
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import url_for
+from flask import redirect
+from flask import make_response
+from flask import session, escape
+import os
+
 
 app = Flask(__name__)
-app.secret_key = os.environ.get("SECRET_KEY")
+app.secret_key = os.urandom(64)
 
-
-@app.route("/")
+@app.route('/')
 def index():
-    pass
+    if 'username' in session:
+        return render_template('index.html', username=session['username'])
+    return redirect(url_for('login'))
+    
 
-
-@app.route("/login", methods=["GET", "POST"])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    pass
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return render_template('login.html')
 
-
-@app.route("/logout", methods=["POST"])
+@app.route('/logout', methods=['POST'])
 def logout():
-    pass
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
